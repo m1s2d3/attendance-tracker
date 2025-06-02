762a892caabc048e9ee47ec8acbc735a592bfe59
m1s2d3 <145163684+m1s2d3@users.noreply.github.com>
2025-06-02 20:05:34 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['app-icon.png'],
+      manifest: {
+        short_name: 'AttendanceTracker',
+        name: 'Attendance Tracker',
+        icons: [
+          {
+            "src": "app-icon.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          }
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#4f46e5',
+        background_color: '#ffffff',
+      },
+    }),
+  ],
+  css: {
+    postcss: './postcss.config.js'
+  }
+});
